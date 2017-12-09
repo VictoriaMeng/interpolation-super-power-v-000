a2af09ef7703e476794b0f6434753a138e1defa3
Victoria Meng <nights1ststar@protonmail.com>
2017-12-09 23:54:48 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  colors.each do |color|
-    if color == colors.last
-      puts "#{color[0].upcase}: #{color}"
-    else
-      print "#{color[0].upcase}: #{color}, "
-    end
-  end
+  puts "R: #{colors[0]}"
 end
 
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
