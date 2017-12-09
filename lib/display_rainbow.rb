b538651adca272fa7904b192f13c09f46a79370e
Victoria Meng <nights1ststar@protonmail.com>
2017-12-09 23:45:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   colors.each do |color|
-    print "#{color[0].upcase}: #{color}, "
+    if color == colors.last
+      print "#{color[0].upcase}: #{color}\n"
+    else
+      print "#{color[0].upcase}: #{color}, "
   end
 end
 
